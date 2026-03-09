03912d9677e3c28aca0ceef4ca7c690a9d13cfef
sagorchowdhury <rajchowdhury820@gmail.com>
2026-03-10 00:53:06 +0400

    added the all categoty btn  from json data

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-console.log("its me")
+console.log("its me");
+
+const url = "https://openapi.programming-hero.com/api/levels/all";
+
+const loadLesson = async () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((allData) => displayLesson(allData.data));
+};
+
+loadLesson();
+
+const displayLesson = (lessons) => {
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  for (let lesson of lessons) {
+    console.log(lesson.level_no);
+
+    const btnDiv = document.createElement("div");
+
+    btnDiv.innerHTML = `
+      <button class="flex items-center gap-2 border px-6 py-3 rounded-lg shadow-sm hover:bg-blue-700 hover:text-white transition">
+        <i class="fa-solid fa-book-open"></i>
+        Lesson ${lesson.level_no}
+      </button>
+    `;
+
+    levelContainer.append(btnDiv);
+  }
+};
